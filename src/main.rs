65922eb58ecbf4a2cb8c8e74fda4d3c49434a31d
Liam <33645555+lj3954@users.noreply.github.com>
2024-07-03 19:42:42 +0100

    feat: Add ChimeraLinux

--- src/main.rs
+++ src/main.rs
@@ -43,6 +43,7 @@ async fn main() {
         spawn(linux::BunsenLabs.to_os()),
         spawn(linux::CachyOS.to_os()),
         spawn(linux::CentOSStream.to_os()),
+        spawn(linux::ChimeraLinux.to_os()),
     ];
 
     let distros = futures::future::join_all(futures)
