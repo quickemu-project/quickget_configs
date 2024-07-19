2dd9cc630eaf961a52137feec93597b26237d8d9
Liam <33645555+lj3954@users.noreply.github.com>
2024-07-18 22:36:44 -0700

    refactor: Change formatting of metadata, options for container images

--- src/container_images.rs
+++ src/container_images.rs
@@ -11,7 +11,17 @@ const TOKEN_SOURCE: &str = r#"https://ghcr.io/token\?scope\="repository:quickemu
 #[derive(Deserialize)]
 struct DockerData {
     url: String,
+    privileged: bool,
+    shared_dirs: Vec<String>,
     data: Vec<DockerOsData>,
+}
+
+#[derive(Deserialize)]
+struct Metadata {
+    privileged: bool,
+    #[serde(default)]
+    shared_dirs: Vec<String>,
+    os_data: Vec<DockerOsData>,
 }
 
 #[derive(Deserialize)]
@@ -30,8 +40,6 @@ struct ImageInfo {
     #[serde(default)]
     edition: Option<String>,
     architectures: Vec<Arch>,
-    #[serde(default)]
-    env: Vec<(String, String)>,
     filename: String,
 }
 
@@ -61,6 +69,7 @@ pub async fn add_container_images(distros: &mut [OS]) {
                     let url = &container.url;
                     let guest_os = os_data.guest_os.clone();
                     let release = image.release + "-build";
+                    let shared_dirs = container.shared_dirs.clone();
                     image.architectures.into_iter().map(move |arch| {
                         let mut config = Config {
                             release: Some(release.clone()),
@@ -71,7 +80,8 @@ pub async fn add_container_images(distros: &mut [OS]) {
                         };
                         let source = Source::Docker(DockerSource {
                             url: url.to_string(),
-                            env: image.env.clone(),
+                            privileged: container.privileged,
+                            shared_dirs: shared_dirs.clone(),
                             output_filename: image.filename.clone(),
                         });
                         match &image.image_type {
@@ -88,7 +98,7 @@ pub async fn add_container_images(distros: &mut [OS]) {
                         config
                     })
                 })),
-                None => eprintln!("OS {} not found in distros", os_data.os_name),
+                None => log::error!("OS {} not found in distros", os_data.os_name),
             }
         })
     });
@@ -114,7 +124,7 @@ async fn fetch_container_images() -> Option<Vec<DockerData>> {
     let tags = api_data.tags;
 
     tags.iter()
-        .flat_map(|tag| DockerData::try_from(&**tag).inspect_err(|e| eprintln!("Failed to parse metadata from tag {tag}: {e}")))
+        .flat_map(|tag| DockerData::try_from(&**tag).inspect_err(|e| log::error!("Failed to parse metadata from tag {tag}: {e}")))
         .collect::<Vec<_>>()
         .into()
 }
@@ -125,10 +135,12 @@ impl<'a> TryFrom<Tag<'a>> for DockerData {
     fn try_from(tag: Tag<'a>) -> Result<Self, Self::Error> {
         let file = format!("packages/{tag}/metadata.json");
         let data = std::fs::read_to_string(file).map_err(|e| e.to_string())?;
-        let images: Vec<DockerOsData> = serde_json::from_str(&data).map_err(|e| e.to_string())?;
+        let images: Metadata = serde_json::from_str(&data).map_err(|e| e.to_string())?;
         Ok(DockerData {
             url: format!("{GH_CONTAINER_SOURCE}:{tag}"),
-            data: images,
+            privileged: images.privileged,
+            shared_dirs: images.shared_dirs,
+            data: images.os_data,
         })
     }
 }
